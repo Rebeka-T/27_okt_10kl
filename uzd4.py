ed5bca0003c7a04122469e7a2ded9b2663cb726e
Rebeka-T <rebeturk@madona.edu.lv>
2025-10-27 11:45:59 +0200

    Update uzd4.py

--- uzd4.py
+++ uzd4.py
@@ -4,6 +4,7 @@ summma=0
 skaitlis=1
 
 while skaitlis<=100:
-    summma=skaitlis # summma=summma+skaitlis
-    skaitlis=1
+    summma+=skaitlis # summma=summma+skaitlis 1.kļ
+    skaitlis+=1  #2.kļ
+
 print("Summa no 1 līdz 100 ir:summma}")
